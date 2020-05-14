875f9dc9e183f9f412e0808bebffe27fa3dc3139
= <=>
2020-05-14 20:21:31 +0200

    Updated with String Concat

--- Source.cpp
+++ Source.cpp
@@ -9,15 +9,23 @@ using namespace std;
 void main()
 {
 	string line;
+	string s;
 	ifstream mf("D:\\College\\Semester 8\\Distributed Computing\\Final Project\\InputSeq.dat.txt");
 	if (mf.is_open())
 	{
 		while (getline(mf, line))
 		{
-			cout << line << '\n';
+			s += line;
+			//cout << line << '\n';
 		}
 		mf.close();
 	}
 	else
+	{
 		cout << "Error";
+	}
+	for (int i = 0; i < 1; i++)
+	{
+		cout << s << '\n';
+	}
 }
